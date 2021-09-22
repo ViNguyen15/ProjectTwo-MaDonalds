0cb24bea145f1a9bf880b55dd1425aff9d7b1829
Joe Linthicum <joelinthicum3@gmail.com>
2021-09-22 14:49:55 -0400

    PlaceOrder cmp product list works

--- force-app/main/default/aura/PlaceOrder/PlaceOrderController.js
+++ force-app/main/default/aura/PlaceOrder/PlaceOrderController.js
@@ -1,9 +1,7 @@
 ({
-    getProductNames : function(component, event, helper) {
-        let productNames = component.get("c.getProductNameList");
-        productNames.setCallback(this, function(resp){
-            component.set("v.productNames", resp.getReturnValue());
-        })
-        $A.enqueueAction(productNames);
+
+    doInit : function(component, event, helper) 
+    {
+        helper.getProductNames(component, event);
     }
 })
--- force-app/main/default/aura/PlaceOrder/PlaceOrderHelper.js
+++ force-app/main/default/aura/PlaceOrder/PlaceOrderHelper.js
@@ -1,5 +1,9 @@
 ({
-    helperMethod : function() {
-
+    getProductNames : function(component, event, helper) {
+        let productNames = component.get("c.getProductNameList");
+        productNames.setCallback(this, function(resp){
+            component.set("v.productNames", resp.getReturnValue());
+        })
+        $A.enqueueAction(productNames);
     }
 })
