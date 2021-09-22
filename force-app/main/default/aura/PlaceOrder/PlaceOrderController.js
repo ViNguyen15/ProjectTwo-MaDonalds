75f533d62e65c717082e2dffd60d89081c2b458f
Joe Linthicum <joelinthicum3@gmail.com>
2021-09-22 12:21:53 -0400

    Fixed some syntax errors

--- force-app/main/default/aura/PlaceOrder/PlaceOrderController.js
+++ force-app/main/default/aura/PlaceOrder/PlaceOrderController.js
@@ -1,9 +1,9 @@
 ({
     getProductNames : function(component, event, helper) {
-        let productNames = compenent.get("c.getProductNameList");
+        let productNames = component.get("c.getProductNameList");
         productNames.setCallback(this, function(resp){
             component.set("v.productNames", resp.getReturnValue());
         })
-        $A.enqueaction(productNames);
+        $A.enqueueAction(productNames);
     }
 })
